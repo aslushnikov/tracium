86cc046c08a936570e7986f8909beae964bd069f
Andrey Lushnikov <lushnikov@chromium.org>
2019-06-18 12:16:10 -0400

    fix: properly sort trace events
    Trace events are not sorted by default. Proper sorting not only uses
    timestamps, but also accounts for trace event duration.
    
    Reference #8

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ const {TestRunner, Reporter, Matchers} = require('@pptr/testrunner');
 const Tracium = require('..');
 
 const readFileAsync = util.promisify(fs.readFile);
-const readJSON = async filePath => {
-  const text = await readFileAsync(path.join(__dirname, filePath), 'utf8');
+const readJSON = async (...filePath) => {
+  const text = await readFileAsync(path.join(__dirname, ...filePath), 'utf8');
   return JSON.parse(text);
 }
 
@@ -16,10 +16,22 @@ const {it, fit, xit} = testRunner;
 const {beforeAll, beforeEach, afterAll, afterEach} = testRunner;
 const {expect} = new Matchers();
 
-describe('Tracium tests', function() {
+describe('Tracium tests against real data', function() {
   it('should work with no FCP', async () => {
     const trace = await readJSON('no_fcp.trace.json');
     expect(Tracium.computeMainThreadTasks(trace).length).toBeGreaterThan(1);
+  });
+  it('should properly sort trace events that have same ts but different dur', async () => {
+    const trace = await readJSON('similar-ts-different-dur.trace.json');
+    expect(Tracium.computeMainThreadTasks(trace).length).toBeGreaterThan(1);
+  });
+});
+
+// Synthetic traces are hand-generated and not real, but tiny and human-manageable.
+describe('Tracium tests against synthetic data', function() {
+  it('should properly sort trace events that have same ts but different dur', async () => {
+    const trace = await readJSON('synthetic', 'similar-ts-different-dur.trace.json');
+    expect(Tracium.computeMainThreadTasks(trace).length).toBe(1);
   });
 });
 
